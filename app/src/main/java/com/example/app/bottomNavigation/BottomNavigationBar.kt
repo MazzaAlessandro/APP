bdf48e1d8f9a1550b3e3dd6fd4dc3ad8591be14f
FISHCAT8 <82520225+Doughet@users.noreply.github.com>
2024-06-02 22:04:37 +0200

    botnavbar

--- app/src/main/java/com/example/app/bottomNavigation/BottomNavigationBar.kt
+++ app/src/main/java/com/example/app/bottomNavigation/BottomNavigationBar.kt
@@ -1,17 +1,23 @@
 package com.example.app.bottomNavigation
 
+import androidx.compose.foundation.border
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.app.ui.theme.PrimaryColor
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,12 +34,24 @@ fun BottomNavigationBar(navController: NavController,
         BottomNavItem.Create
     )
 
-    NavigationBar(modifier = Modifier.testTag("BottomNavigationBar")) {
+    NavigationBar(modifier = Modifier.testTag("BottomNavigationBar"),
+        containerColor = Color.LightGray.copy(alpha = 0.2f)
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
         items.forEach{item->
             NavigationBarItem(
+
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = PrimaryColor, // Color when selected
+                    //unselectedIconColor = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium), // Color when not selected
+                    selectedTextColor = PrimaryColor, // Text color when selected
+                    //unselectedTextColor = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium) // Text color when not selected
+                    indicatorColor = Color.White
+
+                ),
+
                 modifier = Modifier.testTag(item.title),
                 selected = currentRoute == item.screen_route,
                 onClick = {
@@ -56,11 +74,13 @@ fun BottomNavigationBar(navController: NavController,
                 icon = {
                     Icon(
                         imageVector = if (currentRoute == item.screen_route) item.selectedIcon else item.unselectedIcon,
-                        contentDescription = item.title)
+                        contentDescription = item.title
+                    )
                 },
                 label = {
                     Text(text = item.title)
-                },)
+                },
+            )
         }
     }
 }
